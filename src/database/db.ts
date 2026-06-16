28c1c10aed5d950a79215fb47381f8c1e1c7fa77
Mohak Jain <mohakj500@gmail.com>
2026-06-16 20:45:48 +0530

    Schema Template

---
+++ src/database/db.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schemas";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set in the environment");
+}
+
+const sql = neon(databaseUrl);
+export const db = drizzle(sql, { schema });
